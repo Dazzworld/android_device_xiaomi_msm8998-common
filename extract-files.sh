d15f7bc1b39480dfb82ec0bb40573f42064d4993
dianlujitao <dianlujitao@lineageos.org>
2018-12-24 12:04:48 +0800

    msm8998-common: Support Alipay fingerprint payment
     * IFAA manager is based on OnePlusOSS, but adapted for Xiaomi's mlipay
       interface.
     * vendor.xiaomi.hardware.mtdservice@1.0.so is not actually used, thus
       patchelf to drop it rather than shipping a blob.
    
    Change-Id: Idf3d3a8d40245984767f4ef5f60f9fe584e69f21

--- extract-files.sh
+++ extract-files.sh
@@ -102,4 +102,7 @@ sed -i "s|/system/framework/qcrilhook.jar|/vendor/framework/qcrilhook.jar|g" "$Q
 QTI_LIBPERMISSIONS="$COMMON_BLOB_ROOT"/vendor/etc/permissions/qti_libpermissions.xml
 sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" "$QTI_LIBPERMISSIONS"
 
+patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "$COMMON_BLOB_ROOT"/vendor/bin/mlipayd
+patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "$COMMON_BLOB_ROOT"/vendor/lib64/libmlipay.so
+
 "$MY_DIR"/setup-makefiles.sh
